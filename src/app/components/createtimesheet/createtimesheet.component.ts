5befe138d4ae10fac20af7ab9b88519bbbf68fe9
Simmy Sebastian <simmysebastian99@gmail.com>
2023-07-12 11:33:30 +0530

    second commit

--- src/app/components/createtimesheet/createtimesheet.component.ts
+++ src/app/components/createtimesheet/createtimesheet.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 declare var $: any; // Import jQuery
 
 @Component({
@@ -8,14 +7,34 @@ declare var $: any; // Import jQuery
   styleUrls: ['./createtimesheet.component.css']
 })
 export class CreatetimesheetComponent implements OnInit, AfterViewInit {
+  @ViewChild('inputFields') inputFields!: ElementRef<HTMLInputElement[]>;
+
+
   selectedOption: string = '';
-  dropdownOptions: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  dropdownOptions: string[] = ['26-6-23 to 2-7-23', '3-7-23 to 9-7-23', '10-7-23 to 16-7-23', '17-7-23 to 23-7-23', '24-7-23 to 30-7-23', '31-7-23 to 6-8-23'];
+
+  weeks: string[] = []; // for weeks
+  weekDays: number[] = [1, 2, 3, 4, 5, 6, 7];
+  defaultTotalSum: string = '0.00';
+
+  rows: any[] = []; //for add row
 
   ngOnInit() {
     // Initialize popover after the component is initialized
     $(document).ready(() => {
       $('[data-bs-toggle="popover"]').popover();
     });
+    this.addRow();
+  }
+
+  addRow() {
+    this.rows.push({}); // Add an empty object or initialize with default values
+  }
+
+  deleteRow(i: number) {
+    if (i > 0) {
+      this.rows.splice(i, 1); // Remove the row at the specified index
+    }
   }
 
   ngAfterViewInit() {
@@ -24,6 +43,20 @@ export class CreatetimesheetComponent implements OnInit, AfterViewInit {
       $('[data-bs-toggle="popover"]').on('hidden.bs.popover', () => {
         $(this).popover('dispose');
       });
+    });
+
+    const inputs = $('input[type="text"]');
+    const totalSum = $('#totalSum');
+
+    inputs.on('input', () => {
+      let sum = 0;
+      inputs.each((index: number, input: HTMLInputElement) => {
+        const value = parseFloat($(input).val() as string);
+        if (!isNaN(value)) {
+          sum += value;
+        }
+      });
+      totalSum.text(sum.toFixed(2));
     });
   }
 
