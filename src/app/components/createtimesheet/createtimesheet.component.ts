0a3d57d6bef6aea35c12e038b592521dde560a99
Shinto Chandy <shintochandy642@gmail.com>
2023-07-16 20:00:59 +0530

    Chuchudu Fixing -  Adding values in each row, Colm sum, Profile section, calendar, week btn navigation

--- src/app/components/createtimesheet/createtimesheet.component.ts
+++ src/app/components/createtimesheet/createtimesheet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -10,12 +11,16 @@ export class CreatetimesheetComponent implements OnInit, AfterViewInit {
   @ViewChildren('inputField') inputFields!: QueryList<ElementRef<HTMLInputElement>>;
 
   selectedOption: string = '';
-  dropdownOptions: string[] = ['26-6-23 to 2-7-23', '3-7-23 to 9-7-23', '10-7-23 to 16-7-23', '17-7-23 to 23-7-23', '24-7-23 to 30-7-23', '31-7-23 to 6-8-23'];
+  // dropdownOptions: string[] = ['1-Jul-23 to 7-Jul-23', '8-Jul-23 to 14-Jul-23', '15-Jul-23 to 21-Jul-23','22-Jul-23 to 28-Jul-23', '29-Jul-23 to 31-Jul-23'];
 
   rows: any[] = [];
   weekDays: string[] = ['mon 3 july', 'tue 4 july', 'wed 5 july', 'thu 6 july', 'fri 7 july', 'sat 8 july', 'sun 9 july'];
 
-  
+  daysOfWeek: Date[] = [];
+  showFirstHalf: boolean = true;
+  selectedDate: NgbDateStruct;
+  minDate: NgbDate;
+  maxDate: NgbDate;
   weeks: { start: number; end: number }[] = [];
   profileContent: string = '';
   totalSumAll: string = '0.00'; // Declare totalSumAll as a string
@@ -30,17 +35,33 @@ export class CreatetimesheetComponent implements OnInit, AfterViewInit {
     'Holidays'
   ];
 
-  constructor(){
-    
+  constructor(private calendar: NgbCalendar) {
+    const today = this.calendar.getToday();
+    this.selectedDate = today;
+
+    this.showFirstHalf = true;
+    this.minDate = this.calendar.getPrev(this.calendar.getToday(), 'y', 5) as NgbDate; // Update to 10 years before current year
+    this.maxDate = this.calendar.getNext(this.calendar.getToday(), 'y', 5) as NgbDate;
   }
+
   ngOnInit() {
     this.addRow();
+    this.generateWeekButtons();
+
+    const currentDate = this.selectedDate.day;
+
+    // Find the week range that includes the current date
+    const currentWeek = this.weeks.find(week => week.start <= currentDate && week.end >= currentDate);
+
+    if (currentWeek) {
+      this.showDays(currentWeek); // Display the days for the current week range
+    }
   }
 
   ngAfterViewInit() {
     this.calculateTotalSum();
   }
-  
+
 
   // Above contents added my me 
 
@@ -86,11 +107,117 @@ export class CreatetimesheetComponent implements OnInit, AfterViewInit {
     });
   }
 
-  
-  
-
   onSelectionChange(event: any) {
     // Do something with the selected option
     console.log(event.value);
   }
+
+  selectFirstHalf() {
+    this.showFirstHalf = true;
+  }
+
+  selectSecondHalf() {
+    this.showFirstHalf = false;
+  }
+
+  getCurrentMonthName(): string {
+    const month = this.selectedDate?.month;
+    return this.getMonthName(month);
+  }
+
+  getMonthName(month: number): string {
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return monthNames[month - 1];
+  }
+
+  getLastDayOfMonth(): string {
+    const lastDayOfMonth = new Date(this.selectedDate.year, this.selectedDate.month, 0).getDate();
+    return lastDayOfMonth.toString().padStart(2, '0');
+  }
+
+  getDropdownLabel(): string {
+    const startDay = 1;
+    const lastDayOfMonth = new Date(this.selectedDate.year, this.selectedDate.month, 0).getDate();
+    const monthName = this.getMonthName(this.selectedDate.month);
+
+    if (this.showFirstHalf) {
+      const startDate = '01';
+      const endDate = '15';
+      return `${startDate}-${monthName.substr(0, 3)}-${this.selectedDate.year} to ${endDate}-${monthName.substr(0, 3)}-${this.selectedDate.year}`;
+    } else {
+      const startDate = '16';
+      const endDate = lastDayOfMonth.toString().padStart(2, '0');
+      return `${startDate}-${monthName.substr(0, 3)}-${this.selectedDate.year} to ${endDate}-${monthName.substr(0, 3)}-${this.selectedDate.year}`;
+    }
+  }
+
+  generateWeekButtons() {
+    const startDate = this.getDaysOfWeek(this.selectedDate);
+    const weeks: { start: number; end: number; month: string }[] = [];
+  
+    const lastDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0).getDate();
+    let weekStart = 1;
+    let weekEnd = 7;
+  
+    while (weekStart <= lastDayOfMonth) {
+      const monthName = this.getMonthName(startDate.getMonth() + 1);
+      weeks.push({ start: weekStart, end: weekEnd, month: monthName });
+  
+      weekStart = weekEnd + 1;
+      weekEnd = Math.min(weekStart + 6, lastDayOfMonth);
+    }
+    this.weeks = weeks;
+  
+    // Check if the currently selected week is within the generated weeks
+    const currentWeek = weeks.find(week => week.start <= this.selectedDate.day && week.end >= this.selectedDate.day);
+  
+    if (currentWeek) {
+      this.showDays(currentWeek); // Display the days for the current week range
+    }
+  }
+  
+
+  getDaysOfWeek(date: NgbDateStruct): Date {
+    const weekStartDay = 1; // Assuming week starts on Monday
+    const selectedDate = new Date(date.year, date.month - 1, date.day);
+    const selectedDay = selectedDate.getDay();
+
+    const diff = selectedDay >= weekStartDay ? selectedDay - weekStartDay : 7 - (weekStartDay - selectedDay);
+    const weekStartDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() - diff);
+
+    return weekStartDate;
+  }
+
+  isHighlighted(date: NgbDate): boolean {
+    const day = date.day;
+    const currentMonth = date.month;
+    const isFirstHalf = day >= 1 && day <= 15;
+    return isFirstHalf && currentMonth === this.selectedDate.month;
+  }
+
+  isCurrentDate(date: NgbDate): boolean {
+    const currentDate = new Date();
+    return (
+      date.year === currentDate.getFullYear() &&
+      date.month === currentDate.getMonth() + 1 &&
+      date.day === currentDate.getDate()
+    );
+  }
+
+  showDays(week: { start: number, end: number }) {
+    const startDate = this.getDaysOfWeek(this.selectedDate);
+    const startDay = week.start;
+    const endDay = week.end;
+    const daysOfWeek: Date[] = [];
+  
+    for (let i = startDay; i <= endDay; i++) {
+      const date = new Date(startDate.getFullYear(), startDate.getMonth(), i);
+      daysOfWeek.push(date);
+    }
+  
+    this.daysOfWeek = daysOfWeek;
+  }
+  
 }
